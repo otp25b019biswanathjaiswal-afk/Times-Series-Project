bbed43e41524a22809c520875b3301b7e3677eb4
otp25b019biswanathjaiswal-afk <otp25b019.biswanathjaiswal@gmail.com>
2025-12-31 13:49:01 +0530

    Refactor plotly_figure.py for better indicator handling
    Refactor plotly_figure.py to use ta library for indicators and improve code structure.

--- pages/utils/plotly_figure.py
+++ pages/utils/plotly_figure.py
@@ -1,10 +1,12 @@
 import plotly.graph_objects as go
 import dateutil
-import ta
 import datetime
 
-import plotly.graph_objects as go
+from ta.momentum import RSIIndicator
+from ta.trend import SMAIndicator, MACD
 
+
+# -------------------- TABLE --------------------
 def plotly_table(dataframe):
     headerColor = 'grey'
     rowEvenColor = '#f8fafd'
@@ -12,7 +14,7 @@ def plotly_table(dataframe):
 
     fig = go.Figure(data=[go.Table(
         header=dict(
-            values=["<b><b>"] + ["<b>"+str(i)[:30]+"<b>" for i in dataframe.columns],
+            values=["<b></b>"] + ["<b>" + str(i)[:30] + "</b>" for i in dataframe.columns],
             line_color='#0078ff',
             fill_color='#0078ff',
             align='center',
@@ -20,172 +22,158 @@ def plotly_table(dataframe):
             height=35,
         ),
         cells=dict(
-            values=[["<b>"+str(i)+"<b>" for i in dataframe.index]] +
-                    [dataframe[i] for i in dataframe.columns],
-                    fill_color=[[rowOddColor, rowEvenColor] * (len(dataframe) // 2 + 1)],
-            align='left',line_color=['white'],font=dict(color='black', size=15),
+            values=[["<b>" + str(i) + "</b>" for i in dataframe.index]] +
+                   [dataframe[i] for i in dataframe.columns],
+            fill_color=[[rowOddColor, rowEvenColor] * (len(dataframe) // 2 + 1)],
+            align='left',
+            line_color='white',
+            font=dict(color='black', size=15),
         )
     )])
 
     fig.update_layout(height=400, margin=dict(l=0, r=0, t=0, b=0))
     return fig
 
+
+# -------------------- FILTER DATA --------------------
 def filter_data(dataframe, num_period):
     if num_period == '1mo':
         date = dataframe.index[-1] + dateutil.relativedelta.relativedelta(months=-1)
-
     elif num_period == '5d':
         date = dataframe.index[-1] + dateutil.relativedelta.relativedelta(days=-5)
-
     elif num_period == '6mo':
         date = dataframe.index[-1] + dateutil.relativedelta.relativedelta(months=-6)
-
     elif num_period == '1y':
         date = dataframe.index[-1] + dateutil.relativedelta.relativedelta(years=-1)
-
     elif num_period == '5y':
         date = dataframe.index[-1] + dateutil.relativedelta.relativedelta(years=-5)
-
     elif num_period == 'ytd':
-        date = datetime.datetime(dataframe.index[-1].year, 1, 1).strftime('%Y-%m-%d')
-
+        date = datetime.datetime(dataframe.index[-1].year, 1, 1)
     else:
         date = dataframe.index[0]
 
-    return dataframe.reset_index()[dataframe.reset_index()['Date'] > date]
+    df = dataframe.reset_index()
+    return df[df['Date'] > date]
 
-def close_chart(dataframe, num_period = False):
+
+# -------------------- CLOSE PRICE CHART --------------------
+def close_chart(dataframe, num_period=False):
     if num_period:
         dataframe = filter_data(dataframe, num_period)
 
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Open'],
-                             mode='lines',
-                             name='Open', line = dict(width=2, color = '#5ab7ff')))
-
+                             name='Open', line=dict(width=2, color='#5ab7ff')))
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Close'],
-                             mode='lines',
-                             name='Close', line = dict(width=2, color = 'black')))
-
+                             name='Close', line=dict(width=2, color='black')))
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['High'],
-                             mode='lines', name='High', line = dict(width=2, color = '#0078ff')))
-
+                             name='High', line=dict(width=2, color='#0078ff')))
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Low'],
-                             mode='lines', name='Low', line = dict(width=2, color = 'red')))
+                             name='Low', line=dict(width=2, color='red')))
 
     fig.update_xaxes(rangeslider_visible=True)
-
-    fig.update_layout(height = 500, margin=dict(l=0, r=20, t=20, b=0),
-                      plot_bgcolor = 'white', paper_bgcolor = '#f1efff', legend=dict(
-                          yanchor='top',
-                          xanchor='right'
-                      ))
+    fig.update_layout(height=500, plot_bgcolor='white', paper_bgcolor='#f1efff')
 
     return fig
 
+
+# -------------------- CANDLESTICK --------------------
 def candlestick(dataframe, num_period):
-    dataframe = filter_data(dataframe,num_period)
-    fig = go.Figure()
-    fig.add_trace(go.Candlestick(x=dataframe['Date'],
-        open=dataframe['Open'], high=dataframe['High'],
-        low=dataframe['Low'], close=dataframe['Close']))
+    dataframe = filter_data(dataframe, num_period)
 
-    fig.update_layout(showlegend = False,height = 500,margin=dict(l=0, r=20, t=20, b=0), plot_bgcolor = 'white', paper_bgcolor = '#e1efff')
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(
+        x=dataframe['Date'],
+        open=dataframe['Open'],
+        high=dataframe['High'],
+        low=dataframe['Low'],
+        close=dataframe['Close']
+    ))
+
+    fig.update_layout(height=500, showlegend=False,
+                      plot_bgcolor='white', paper_bgcolor='#e1efff')
     return fig
 
+
+# -------------------- RSI --------------------
 def RSI(dataframe, num_period):
-    dataframe['RSI'] = pta.rsi(dataframe['Close'])
-    dataframe = filter_data(dataframe,num_period)
+    rsi = RSIIndicator(close=dataframe['Close'], window=14)
+    dataframe['RSI'] = rsi.rsi()
+
+    dataframe = filter_data(dataframe, num_period)
+
     fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=dataframe['Date'],
-        y=dataframe.RSI, name = 'RSI',marker_color='orange',line = dict( width=2,color = 'orange'),
-    ))
-    fig.add_trace(go.Scatter(
-        x=dataframe['Date'],
-        y=[70]*len(dataframe), name = 'Overbought', marker_color='red',line = dict( width=2,color = 'red',dash='dash'),
-    ))
-    fig.add_trace(go.Scatter(
-        x=dataframe['Date'],
-        y=[30]*len(dataframe), fill='tonexty', name = 'Oversold', marker_color='#79da84',line = dict( width=2,color = '#79da84',dash='dash')
-    ))
-    fig.update_layout(yaxis_range=[0,100],
-        height=200,plot_bgcolor = 'white', paper_bgcolor = '#e1efff',margin=dict(l=0, r=0, t=0, b=0),legend=dict(orientation="h",
-        yanchor="top",
-        y=1.02,
-        xanchor="right",
-        x=1
-    ))
+
+    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['RSI'],
+                             name='RSI', line=dict(width=2, color='orange')))
+    fig.add_trace(go.Scatter(x=dataframe['Date'], y=[70]*len(dataframe),
+                             name='Overbought', line=dict(dash='dash', color='red')))
+    fig.add_trace(go.Scatter(x=dataframe['Date'], y=[30]*len(dataframe),
+                             name='Oversold', line=dict(dash='dash', color='green')))
+
+    fig.update_layout(height=200, yaxis_range=[0, 100],
+                      plot_bgcolor='white', paper_bgcolor='#e1efff')
     return fig
 
-def Moving_average(dataframe,num_period):
 
-    dataframe['SMA_50'] = pta.sma(dataframe['Close'],50)
-    dataframe = filter_data(dataframe,num_period)
+# -------------------- MOVING AVERAGE --------------------
+def Moving_average(dataframe, num_period):
+    sma = SMAIndicator(close=dataframe['Close'], window=50)
+    dataframe['SMA_50'] = sma.sma_indicator()
+
+    dataframe = filter_data(dataframe, num_period)
+
     fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Open'],
-                mode='lines',
-                name='Open', line = dict( width=2,color = '#5ab7ff')))
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Close'],
-                mode='lines',
-                name='Close', line = dict( width=2,color = 'black')))
-    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['High'],
-                mode='lines', name='High', line = dict( width=2,color = '#0078ff')))
-    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['Low'],
-                mode='lines', name='Low', line = dict( width=2,color = 'red')))
+                             name='Close', line=dict(color='black')))
     fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['SMA_50'],
-                mode='lines', name='SMA 50', line = dict( width=2,color = 'purple')))
+                             name='SMA 50', line=dict(color='purple')))
 
     fig.update_xaxes(rangeslider_visible=True)
-    fig.update_layout(height = 500,margin=dict(l=0, r=20, t=20, b=0), plot_bgcolor = 'white',paper_bgcolor = '#e1efff',legend=dict(
-        yanchor="top",
-        xanchor="right"
-    ))
+    fig.update_layout(height=500, plot_bgcolor='white', paper_bgcolor='#e1efff')
     return fig
 
-def MACD(dataframe, num_period):
-    macd = pta.macd(dataframe['Close']).iloc[:,0]
-    macd_signal = pta.macd(dataframe['Close']).iloc[:,1]
-    macd_hist = pta.macd(dataframe['Close']).iloc[:,2]
-    dataframe['MACD'] = macd
-    dataframe['MACD Signal'] = macd_signal
-    dataframe['MACD Hist'] = macd_hist
-    dataframe = filter_data(dataframe,num_period)
+
+# -------------------- MACD --------------------
+def MACD_plot(dataframe, num_period):
+    macd = MACD(close=dataframe['Close'])
+
+    dataframe['MACD'] = macd.macd()
+    dataframe['MACD Signal'] = macd.macd_signal()
+    dataframe['MACD Hist'] = macd.macd_diff()
+
+    dataframe = filter_data(dataframe, num_period)
+
     fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=dataframe['Date'],
-        y=dataframe['MACD'], name = 'RSI',marker_color='orange',line = dict( width=2,color = 'orange'),
-    ))
-    fig.add_trace(go.Scatter(
-        x=dataframe['Date'],
-        y=dataframe['MACD Signal'], name = 'Overbought', marker_color='red',line = dict( width=2,color = 'red',dash='dash'),
-    ))
-    c = ['red' if cl <0 else "green" for cl in macd_hist]
+    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['MACD'],
+                             name='MACD', line=dict(color='blue')))
+    fig.add_trace(go.Scatter(x=dataframe['Date'], y=dataframe['MACD Signal'],
+                             name='Signal', line=dict(color='red')))
 
-    fig.update_layout(
-        height=200,plot_bgcolor = 'white', paper_bgcolor = '#e1efff',margin=dict(l=0, r=0, t=0, b=0),legend=dict(orientation="h",
-        yanchor="top",
-        y=1.02,
-        xanchor="right",
-        x=1
-    ))
+    fig.update_layout(height=250, plot_bgcolor='white', paper_bgcolor='#e1efff')
     return fig
 
+
+# -------------------- FORECAST CHART --------------------
 def Moving_average_forecast(forecast):
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=forecast.index[:-30], y=forecast['Close'].iloc[:-30],
-                mode='lines',
-                name='Close Price', line = dict( width=2,color = 'black')))
-    fig.add_trace(go.Scatter(x=forecast.index[-31:], y=forecast['Close'].iloc[-31:],
-                mode='lines', name='Future Close Price', line = dict( width=2,color = 'red')))
+    fig.add_trace(go.Scatter(
+        x=forecast.index[:-30],
+        y=forecast['Close'].iloc[:-30],
+        name='Close Price',
+        line=dict(color='black')
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=forecast.index[-30:],
+        y=forecast['Close'].iloc[-30:],
+        name='Future Price',
+        line=dict(color='red')
+    ))
 
     fig.update_xaxes(rangeslider_visible=True)
-    fig.update_layout(height = 500,margin=dict(l=0, r=20, t=20, b=0), plot_bgcolor = 'white',paper_bgcolor = '#e1efff',legend=dict(
-        yanchor="top",
-        xanchor="right"
-    ))
+    fig.update_layout(height=500, plot_bgcolor='white', paper_bgcolor='#e1efff')
 
     return fig
